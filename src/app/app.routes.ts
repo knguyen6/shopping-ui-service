d0ab2f707ef30d81a998c7d7dfd04bed6289fb2b
Khanh Nguyen <khanh.nguyen@internetidentity.com>
2016-12-01 20:54:23 -0800

    UI for checkout service

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+//
+// import { ModuleWithProviders }  from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { ListViewComponent } from './listview/listview.component';
+// import { CheckoutComponent } from './checkout/checkout.component';
+// import { CartComponent } from './cart/cart.component';
+// import { ErrorhandlerComponent } from './errorhandler/errorhandler.component';
+// import { AppComponent } from './app.component';
+//
+// // Route Configuration
+// export const routes: Routes = [
+//   { path: '', component: AppComponent },
+//   { path: 'cart', component: CartComponent },
+//   { path: 'checkout', component: CheckoutComponent },
+//   { path: 'listview', component: ListViewComponent },
+//   { path: 'confirmation', component: ErrorhandlerComponent }
+// ];
+//
+// export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
