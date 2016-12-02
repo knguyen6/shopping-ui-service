09d865c58c767978372454f1402710f39b6e5ffc
Inno Estrera <inno.irving.estrera@gmail.com>
2016-12-02 00:09:30 -0800

    Routing implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {CheckoutComponent} from "./checkout/checkout.component";
 import {ListviewComponent} from "./listview/listview.component";
 import {Routes, RouterModule} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
+import {CartComponent} from "./cart/cart.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ListviewComponent },
+  { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'confirmation', component: ConfirmationComponent }
 ];
